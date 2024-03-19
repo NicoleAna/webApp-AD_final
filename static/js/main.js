34592949336698ea3e3e3281163e3fed8592b19b
varad0207 <93907658+varad0207@users.noreply.github.com>
2024-03-19 23:05:43 +0530

    styling

--- static/js/main.js
+++ static/js/main.js
@@ -6,3 +6,24 @@ function toggleCheckboxes() {
         checkbox.checked = selectAllCheckbox.checked;
     }); 
 }
+
+const dataItems = document.querySelectorAll('.data-item');
+
+dataItems.forEach(item => {
+    // item.addEventListener('click', () => {
+    //     const isItemOpen = item.classList.contains('active');
+    //     dataItems.forEach(item => item.classList.remove('active'));
+    //     if(!isItemOpen) {
+    //         item.classList.toggle('active');
+    //     }
+    // });
+
+    item.addEventListener('mouseover', () => {
+        dataItems.forEach(item => item.classList.remove('active'));
+        item.classList.add('active');
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.classList.remove('active');
+    });
+});
